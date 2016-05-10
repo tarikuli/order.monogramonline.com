b894e93324b5351bf1b9eb0ff3ce9f992bbdac99
ssi-anik <sirajul.islam.anik@gmail.com>
2016-05-11 01:41:27 +0600

    Update items

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,11 +24,23 @@ class HomeController extends Controller
 
 	public function test (Request $request)
 	{
-		$items = Item::all();
-		return $items;
-		foreach ( $items as $item ) {
-			$child_sku = Helper::getChildSku($item);
-			return $child_sku;
+
+		foreach ( range(0, 1000) as $count ) {
+			$items = Item::where('is_deleted', '0')
+						 ->take(1000)
+						 ->skip($count * 1000)
+						 ->get();
+			if ( $items->count() == 0 ) {
+				return sprintf("<b>Finish at: %s</b>", date('Y-m-d h:m:s'));
+				break;
+			}
+			echo sprintf("<i>Started %05d at: %s<br/></i>", $items->first()->id, date('Y-m-d h:m:s'));
+			foreach ( $items as $item ) {
+				$child_sku = Helper::getChildSku($item);
+				$item->child_sku = $child_sku;
+				$item->save();
+			}
 		}
+
 	}
 }
