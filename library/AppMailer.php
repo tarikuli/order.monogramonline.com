9ba6c222f6a1aff73d5f5070c02fa3e17c1d73d4
Tarikul <tarikuli@gmail.com>
2016-06-24 16:36:32 -0400

    Test bcc

--- library/AppMailer.php
+++ library/AppMailer.php
@@ -107,8 +107,8 @@ class AppMailer
 		$this->sender_name = env("APPLICATION_NAME");
 		$this->subject = $subject;
 		$this->to = $bill_email;
-		$this->cc = "shlomi@monogramonline.com";
-		$this->bcc = "jewel@monogramonline.com";
+		$this->cc = 'shlomi@monogramonline.com';
+		$this->bcc = 'jewel@monogramonline.com';
 		$this->view = 'emails.shippingconfirm';
 		$this->data = compact('modules');
 		$this->deliver();
