b0f3fd41fb4ae2598b3b844fc3a7aa0bf08d8dc1
tarikuli@gmail.com <tarikuli@gmail.com>
2017-01-09 10:39:21 -0500

    name show

--- resources/views/includes/header_menu.blade.php
+++ resources/views/includes/header_menu.blade.php
@@ -12,7 +12,6 @@
         </div>
 
         <div class = "collapse navbar-collapse" id = "bs-example-navbar-collapse-1">
-        Now Login {{ (auth()->user()->username) }} 
             <ul class = "nav navbar-nav navbar-right">
           
                <li><a href = "{{url('logout')}}"><i class = "fa fa-sign-out"></i>  {{ (auth()->user()->username) }}  Logout </a></li>
